75649b636491bc6da9ceb9ead261da02c1f5f36e
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2021-12-17 16:38:20 +0200

    Fix non-ISO-standard escape sequence, '\e'

--- src/colors.h
+++ src/colors.h
@@ -7,15 +7,15 @@
 	Used to change the color of the lines writted to the user's console.
 	Check: https://en.wikipedia.org/wiki/ANSI_escape_code
 */
-const std::string Black 	= "\e[1;30m";
-const std::string Red 		= "\e[1;31m";
-const std::string Green 	= "\e[1;32m";
-const std::string Yellow	= "\e[1;33m";
-const std::string Blue  	= "\e[1;34m";
-const std::string Purple	= "\e[1;35m";
-const std::string Cyan  	= "\e[1;36m";
-const std::string White 	= "\e[1;37m";
-const std::string Reset		= "\e[0m";
+const std::string Black 	= "\033[1;30m";
+const std::string Red 		= "\033[1;31m";
+const std::string Green 	= "\033[1;32m";
+const std::string Yellow	= "\033[1;33m";
+const std::string Blue  	= "\033[1;34m";
+const std::string Purple	= "\033[1;35m";
+const std::string Cyan  	= "\033[1;36m";
+const std::string White 	= "\033[1;37m";
+const std::string Reset		= "\033[0m";
 
 #endif // COLORS_H
 
